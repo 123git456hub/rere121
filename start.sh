b8a813014a805976c980b8de9dc3f52827cc5cff
123git456hub <97772244+123git456hub@users.noreply.github.com>
2022-11-06 01:31:46 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 export UUID=93e61de6-57de-46d8-b93d-380c15202afc
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.bing.com
+export ProxySite=github.com/StartBootstrap/startbootstrap-new-age/archive/master.zip
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
