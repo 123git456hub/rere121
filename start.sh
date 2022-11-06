fa31a8af752cfe9029e07dc90fa872c32d1b440c
123git456hub <97772244+123git456hub@users.noreply.github.com>
2022-11-06 00:12:55 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=93e61de6-57de-46d8-b93d-380c15202afc
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=www.bing.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
